7fb27f44f0962dce4da66ef3f49bb98522020b27
Rachel T <rachmaybe@gmail.com>
2019-08-30 01:57:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,14 @@
-Class Person 
-  def intialize(name)
+class Person
+  def initialize (persons_name)
+    @name = persons_name
+  end
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name
     @name
-  end 
-end 
+  end
+  
+end
