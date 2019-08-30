72293e7ca53d12d99441555faddd2dc12761f84d
Rachel T <rachmaybe@gmail.com>
2019-08-30 01:58:47 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,44 @@
+class Dog
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end 
+end 
+  
+  fido = Dog.new("Fido") 
+  
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+end
+fido = Dog.new("Fido")
+
+class Baby
+  def initialize 
+    cry 
+  end 
+    
+    def cry
+      puts "waaaah"
+    end 
+    def name=(the_baby_name)   
+      @my_name = the_baby_name
+    end 
+    def name  
+      @my_name 
+    end 
+end 
