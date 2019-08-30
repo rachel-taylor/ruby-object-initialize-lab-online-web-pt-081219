0ac1b55690e9b0c77fd296ae6db8c113b9818783
Rachel T <rachmaybe@gmail.com>
2019-08-30 01:57:54 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,46 @@
+class Dog
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end 
+end 
+  
+  fido = Dog.new("Fido") # => #<Dog:0x00564f20b62040 @name="Fido", @breed="Mutt">
+  
+  # github solutions:
+  
+class Dog
+  def initialize(name, breed = "Mut")
+    @name = name
+    @breed = breed
+  end
+end
+fido = Dog.new("Fido")
+
+class Baby
+  def initialize 
+    cry 
+  end 
+    
+    def cry
+      puts "waaaah"
+    end 
+    def name=(the_baby_name)   
+      @my_name = the_baby_name
+    end 
+    def name  
+      @my_name 
+    end 
+end 
